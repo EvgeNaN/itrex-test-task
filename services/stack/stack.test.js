76cc63c0c10988ca071a1b69cf2025b0eca309d5
Evgeniy Ryabchenko <evgeniy.ryabchenko.175@gmail.com>
2024-12-04 19:44:32 +0100

    added tests for stack

---
+++ services/stack/stack.test.js
@@ -0,0 +1,55 @@
+const { ERRORS } = require('./constants');
+
+const Stack = require('./index');
+
+describe('Stack tests', () => {
+  let stack;
+
+  beforeEach(() => {
+    stack = new Stack();
+  });
+
+  afterEach(() => {
+    stack = null;
+  });
+
+  it('Should return top value', () => {
+    stack.push('Hello');
+    stack.push('World');
+
+    expect(stack.pop()).toBe('World');
+
+    stack.push('Again');
+
+    expect(stack.pop()).toBe('Again');
+    expect(stack.pop()).toBe('Hello');
+    expect(stack.pop()).toBeUndefined();
+  });
+
+  it('Should throw exception if no value provided', () => {
+    try {
+      stack.push();
+
+      expect(true).toBe(false);
+    } catch (e) {
+      expect(e.message).toBe(ERRORS.VALUE_REQUIRED().message);
+    }
+  });
+
+  it('Should throw exception if overflow', () => {
+    stack = new Stack({
+      maxSize: 2,
+    });
+
+    stack.push(1);
+    stack.push(2);
+
+    try {
+      stack.push(3);
+
+      expect(true).toBe(false);
+    } catch (e) {
+      expect(e.message).toBe(ERRORS.OVERFLOW().message);
+    }
+  });
+});
